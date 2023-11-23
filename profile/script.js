332e9d543e9c83cb7b65cead67537b20f9bb8d8d
B1gels <fikar992@gmail.com>
2023-11-23 15:34:01 +0800

    update baru

--- profile/script.js
+++ profile/script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+console.log("hello world");
+function tampilkanGambar(id){
+// sembunyikan semua gambar 
+document.querySelectorAll('.skinimg').forEach(img => {
+    img.style.opacity = 0;
+    img.style.transition = 'opacity 1.5s ease-in-out';
+
+});
+
+// menampilkan gambar terpilih setelah 1.5 detik 
+setTimeout(() => {
+    document.getElementById('gambar1').style.display = id === 'gambar1' ? 'block' : 'none';
+    document.getElementById('gambar2').style.display = id === 'gambar2' ? 'block' : 'none';
+    document.getElementById('gambar3').style.display = id === 'gambar3' ? 'block' : 'none';
+    document.getElementById('gambar4').style.display = id === 'gambar4' ? 'block' : 'none';
+
+    //set opacity untuk animasi masuk
+    document.querySelectorAll('.skinimg').forEach(img => {
+        img.style.opacity = 1;
+    });
+}, 400);
+};
